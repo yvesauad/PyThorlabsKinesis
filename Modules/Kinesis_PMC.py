cccd663baab3c04166c2a61420f9892f9ac32f4c
yvesauad <yvesauad@gmail.com>
2021-07-31 12:32:36 +0200

    Organizating files. Working example commented

--- Modules/Kinesis_PMC.py
+++ Modules/Kinesis_PMC.py
@@ -1,6 +1,5 @@
-from ctypes import cdll, POINTER
-from ctypes import c_uint, c_char_p, c_void_p, c_ushort, c_short, c_int, c_long, \
-    c_ulong, c_bool, WINFUNCTYPE
+from ctypes import cdll, POINTER, c_uint, c_char, c_byte, c_char_p, c_void_p, c_ushort, c_short, c_int, c_long, \
+    c_ulong, c_bool, WINFUNCTYPE, Structure
 
 from Modules.ErrorEnum import FTDI_COM_ERROR
 
@@ -17,8 +16,18 @@ def c_str_array(strings):
     arr[:] = strings
     return arr
 
+class TLI_HardwareInformation(Structure):
+    _fields_ = [("serialNumber", c_ulong),
+                ("modelNumber", (8 * c_char)),
+                ("type", c_ushort),
+                ("firmwareVersion", c_ulong),
+                ("notes", (48 * c_char)),
+                ("deviceDependantData", (12 * c_byte)),
+                ("hardwareVersion", c_ushort),
+                ("modificationState", c_ushort),
+                ("numChannels", c_short)]
+
 LOGGERFUNC = WINFUNCTYPE(None, c_void_p)
-TIMEOUT = 10
 
 class TLKinesisPiezoMotorController():
 
@@ -52,12 +61,19 @@ class TLKinesisPiezoMotorController():
         self.__RegisterMessageCallback = _buildFunction(_library.KIM_RegisterMessageCallback, [c_char_p, LOGGERFUNC], c_void_p)
         self.__GetNextMessage = _buildFunction(_library.KIM_GetNextMessage, [c_char_p, POINTER(c_ulong), POINTER(c_ulong),
                                                                              POINTER(c_ulong)], c_bool)
+        self.__GetHardwareInfoBlock = _buildFunction(_library.KIM_GetHardwareInfoBlock, [c_char_p,
+                                                                                        POINTER(TLI_HardwareInformation)], c_short)
+        self.__GetDriveOPParameters = _buildFunction(_library.KIM_GetDriveOPParameters,
+                                                     [c_char_p, c_ushort, POINTER(c_short), POINTER(c_int), POINTER(c_int)], c_short)
+        self.__SetDriveOPParameters = _buildFunction(_library.KIM_SetDriveOPParameters,
+                                                     [c_char_p, c_ushort, c_short, c_int, c_int], c_short)
 
-    def __init__(self, serialno, pollingTime = 100):
+    def __init__(self, serialno, pollingTime = 100, TIMEOUT = 5):
         self._initialize_library()
         self.__serial = serialno.encode()
         self.__fn = LOGGERFUNC(self._callback)
         self.__eventHandler = threading.Event()
+        self.__timeout = TIMEOUT
         self.BuildDeviceList()
         self.OpenConnection()
         self.StartPolling(pollingTime)
@@ -69,8 +85,6 @@ class TLKinesisPiezoMotorController():
     def _callback(self, p):
         res = self.GetNextMessage()
         if self.__pos == self.GetCurrentPositionAll():
-            print(f'Message is {res}. Hardware position is {self.GetCurrentPositionAll()}. '
-                  f'Soft. position is {self.__pos}')
             self.__eventHandler.set()
 
     def updatePosition(self):
@@ -105,6 +119,14 @@ class TLKinesisPiezoMotorController():
         """
         return self._error_check(self.__Open(self.__serial))
 
+    def UpdatePosition(self):
+        """
+
+        :return: None
+        """
+        self.__pos = self.GetCurrentPositionAll()
+        return
+
     def MoveRelative(self, channel: int, step: int):
         """
 
@@ -116,7 +138,9 @@ class TLKinesisPiezoMotorController():
             self.__eventHandler.clear()
             self.__pos[channel - 1] += step
             self._error_check(self.__MoveRelative(self.__serial, channel, step))
-            self.__eventHandler.wait(TIMEOUT)
+            if not self.__eventHandler.wait(self.__timeout):
+                print('Timeout achieved. Updating position to the current position.')
+                self.updatePosition()
             return True
         return False
 
@@ -131,7 +155,9 @@ class TLKinesisPiezoMotorController():
             self.__eventHandler.clear()
             self.__pos[channel - 1] = value
             self._error_check(self.__MoveAbsolute(self.__serial, channel, value))
-            self.__eventHandler.wait(TIMEOUT)
+            if not self.__eventHandler.wait(self.__timeout):
+                print('Timeout achieved. Updating position to the current position.')
+                self.updatePosition()
             return True
         return False
 
@@ -211,3 +237,52 @@ class TLKinesisPiezoMotorController():
         msg_data = c_ulong(0x00)
         res = self.__GetNextMessage(self.__serial, msg_type, msg_id, msg_data)
         return (msg_type.value, msg_id.value, msg_data.value, res)
+
+    def GetHardwareInfoBlock(self):
+        """
+
+        :return: Dict
+        """
+        value = TLI_HardwareInformation()
+        self._error_check(self.__GetHardwareInfoBlock(self.__serial, value))
+        dict = {
+            "Serial Number": value.serialNumber,
+            "Model Number": value.modelNumber,
+            "Type": value.type,
+            "Firmware Version": value.firmwareVersion,
+            "Notes": value.notes,
+            "Device Data": value.deviceDependantData,
+            "Hardware Version": value.hardwareVersion,
+            "Modification State": value.modificationState,
+            "Number of channels": value.numChannels
+        }
+        return dict
+
+    def GetDriveOPParameters(self, channel: int):
+        """
+
+        :param channel: int
+        :return: Dict
+        """
+        voltage = c_short(0x00)
+        sr = c_int(0x00)
+        sa = c_int(0x00)
+        self._error_check(self.__GetDriveOPParameters(self.__serial, channel, voltage, sr, sa))
+        dict = {
+            "Voltage": voltage.value,
+            "Step Rate": sr.value,
+            "Step Acceleration": sa.value
+        }
+        return dict
+
+    def SetDriveOPParameters(self, channel: int, voltage: int, step_rate: int, step_acc: int):
+        """
+
+        :param channel: int
+        :param voltage: int
+        :param step_rate: int
+        :param step_acc: int
+        :return: None
+        """
+        self._error_check(self.__SetDriveOPParameters(self.__serial, channel, voltage, step_rate, step_acc))
+        return
--- RunningTest.py
+++ RunningTest.py
@@ -1,6 +1,31 @@
 from Modules import Kinesis_PMC
 
-my_piezo = Kinesis_PMC.TLKinesisPiezoMotorController('97101311')
+"""
+Instantiate my piezo controller. 
+First string is the serial number
+Polling time is the feedback message period. 
+Timeout is the maximum time delay.
+"""
+my_piezo = Kinesis_PMC.TLKinesisPiezoMotorController('97101311', pollingTime=100, TIMEOUT=1.5)
 
-val = 150
+"""
+Getting a few information from the piezo. 
+"""
+hardwareInfo = my_piezo.GetHardwareInfoBlock()
+piezoInfo = my_piezo.GetDriveOPParameters(1)
+print(hardwareInfo)
+print(piezoInfo)
+
+"""
+Setting the piezo moving conditions. Max voltage is 112 moving at 250 steps/s and 1000 steps/s^2 acceleration.
+Move all four channels a value of 100 relative to the initial value.
+Move all four channels to the origin of the system.
+Update the python position array with the hardware value. This is important to keep track between software and hardware
+and to being as responsive as possible i.e. if (soft position == hardware position) => thread is set.
+"""
+val = 250
+my_piezo.SetDriveOPParameters(1, 112, 500, 1000)
 [my_piezo.MoveRelative(x+1, val) for x in range(4)]
+[my_piezo.MoveAbsolute(x+1, 0) for x in range(4)]
+my_piezo.UpdatePosition()
+
